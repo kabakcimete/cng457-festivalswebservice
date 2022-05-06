5510c923d1967f0c396d2020b7c4df269fc45562
Maviatack <32220694+Maviatack@users.noreply.github.com>
2022-05-07 00:09:09 +0300

    Three Service files are added.

---
+++ src/main/java/com/example/festivalswebservice/service/FestivalService.java
@@ -0,0 +1,8 @@
+package com.example.festivalswebservice.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalService {
+}
