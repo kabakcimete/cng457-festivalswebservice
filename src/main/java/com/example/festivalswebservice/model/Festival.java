fc12e11532a61afedec732e5d8664fbb9ff3df77
mete kabakci <kabakcimete@gmail.com>
2022-05-06 23:23:55 +0300

    Initial commit, spring project has been created. Sorry hocam we had to create another repository :)

---
+++ src/main/java/com/example/festivalswebservice/model/Festival.java
@@ -0,0 +1,5 @@
+package com.example.festivalswebservice.model;
+
+public class Festival {
+
+}
---
+++ src/main/java/com/example/festivalswebservice/service/FestivalswebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.festivalswebservice.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalswebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalswebserviceApplication.class, args);
+	}
+
+}
