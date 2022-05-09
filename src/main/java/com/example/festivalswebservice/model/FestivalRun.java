9ac0916869be35bb6b13f23860916caa86fbec94
mete kabakci <kabakcimete@gmail.com>
2022-05-10 01:50:25 +0300

    Models are implemented

--- src/main/java/com/example/festivalswebservice/model/FestivalRun.java
+++ src/main/java/com/example/festivalswebservice/model/FestivalRun.java
@@ -1,4 +1,35 @@
 package com.example.festivalswebservice.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "FestivalRunId")
 public class FestivalRun {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="frid")
+    private int FestivalRunId;
+    private String Name;
+    private int Duration;
+    private Date Date;
+    @OneToMany
+    private Set<Event> Events;
+    @ManyToMany
+    private Set<Organizer> Organizers;
+    @ManyToOne
+    private Festival festival;
 }
--- src/main/java/com/example/festivalswebservice/model/Show.java
+++ src/main/java/com/example/festivalswebservice/model/Show.java
@@ -1,4 +1,18 @@
 package com.example.festivalswebservice.model;
 
-public class Show {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "EventId")
+public class Show extends Event{
+    private String  Performers;
 }
