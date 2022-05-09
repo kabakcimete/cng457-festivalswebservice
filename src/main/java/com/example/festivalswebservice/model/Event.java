a7f5f995b10a6e4629210ce9f3c6a5e0bd96f65d
mete kabakci <kabakcimete@gmail.com>
2022-05-10 01:50:25 +0300

    All controllers are added now they are ready to implementation

--- src/main/java/com/example/festivalswebservice/model/Event.java
+++ src/main/java/com/example/festivalswebservice/model/Event.java
@@ -1,4 +1,31 @@
 package com.example.festivalswebservice.model;
 
-public class Event {
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "EventId")
+public  class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="eid")
+    private int EventId;
+    private String name;
+    private String Description;
+    private Date Date;
+    @OneToOne
+    private FestivalRun Festivalrun;
+
 }
