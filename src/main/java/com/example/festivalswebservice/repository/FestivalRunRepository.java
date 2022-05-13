0f756e2d765768e5498aba07d20e655a33a6ad87
Maviatack <32220694+Maviatack@users.noreply.github.com>
2022-05-13 11:06:34 +0300

    javadoc of repositories are modified.

--- src/main/java/com/example/festivalswebservice/repository/FestivalRunRepository.java
+++ src/main/java/com/example/festivalswebservice/repository/FestivalRunRepository.java
@@ -8,10 +8,12 @@ import java.util.List;
 
 public interface FestivalRunRepository  extends JpaRepository<FestivalRun, Integer> {
     /**
+     * @param x is the integer id of the FestivalRun
      * @return FestivalRun which have the given id number of input variable x
      * */
     public FestivalRun findByFestivalRunId(int x);
     /**
+     * @param i is the duration time with the integer type.
      * @return list of FestivalRuns with the duration number less than the input variable i
      * */
     public List<FestivalRun> findByDurationLessThan(int i);
