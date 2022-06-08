0297c5c853e76b7438023f728040f49229700490
mete kabakci <kabakcimete@gmail.com>
2022-06-08 21:34:31 +0300

    End point2 correction

--- src/main/java/com/example/festivalswebservice/repository/OrganizerRepository.java
+++ src/main/java/com/example/festivalswebservice/repository/OrganizerRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface OrganizerRepository  extends JpaRepository<Organizer, String> {
 
-    public List<Organizer> findByEmailContainsOrFnameContainsAndLnameContainsOrEmailContains(String d);
+    public List<Organizer> findByEmailContainsOrFnameContainsOrLnameContainsOrPhoneContains(String key1,String key2,String key3,String key4);
 
     @Query("SELECT o FROM Organizer o WHERE o.festivals.size<1")
     public List<Organizer> organisersWithMultipleRuns();
