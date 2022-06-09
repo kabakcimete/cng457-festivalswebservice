cca5aa30bfea2f0a174af142c2fd1582765d7ffd
mete kabakci <kabakcimete@gmail.com>
2022-06-09 22:20:05 +0300

    Unit Tests

--- src/main/java/com/example/festivalswebservice/service/OrganizerService.java
+++ src/main/java/com/example/festivalswebservice/service/OrganizerService.java
@@ -23,7 +23,7 @@ public class OrganizerService {
     }
 
     public List<Organizer> organiserSearch(String d){
-        return organizerRepository.findByEmailContainsOrFnameContainsOrLnameContainsOrPhoneContains(d,d,d,d);
+        return organizerRepository.findByEmailStartingWithOrFnameContainsOrLnameContainsOrPhoneEquals(d,d,d,d);
     }
 
     public List<Organizer> organisersWithMultipleRuns(){
